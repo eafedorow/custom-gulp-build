9328e43e84bc1eeb18101ced5d4488a333c62d57
eafed <eafedorow@yandex.ru>
2022-08-25 20:15:53 +0300

    img compression

--- gulpfile.js
+++ gulpfile.js
@@ -5,11 +5,20 @@ const cleanCSS = require('gulp-clean-css');
 const babel = require('gulp-babel')
 const uglify = require('gulp-uglify');
 const concat = require('gulp-concat')
-const del = require('del');
 const sourcemaps = require('gulp-sourcemaps');
 const autoprefixer = require('gulp-autoprefixer');
+const imagemin = require('gulp-imagemin')
+const del = require('del');
+const size = require('gulp-size');
+const newer = require('gulp-newer');
+const browsersync = require('browser-sync');
+const htmlmin = require('gulp-htmlmin');
 
 const paths = {
+  html: {
+    src: 'src/**/*.html',
+    dest: 'dist/'
+  },
   styles: {
     src: 'src/styles/**/*.less',
     dest: 'dist/css/'
@@ -17,11 +26,23 @@ const paths = {
   scripts: {
     src: 'src/scripts/**/*.js',
     dest: 'dist/js/'
+  }, 
+  images: {
+    src: 'src/img/**',
+    dest: 'dist/img/'
   }
 }
 
 function clean() {
-  return del(['dist'])
+  return del(['dist/*', '!dist/img'])
+}
+
+function html() {
+  return gulp.src(paths.html.src)
+    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(size())
+    .pipe(gulp.dest(paths.html.dest))
+    .pipe(browsersync.stream())
 }
 
 function styles() {
@@ -40,7 +61,9 @@ function styles() {
       extname: '.css'
     }))
     .pipe(sourcemaps.write('.'))
+    .pipe(size())
     .pipe(gulp.dest(paths.styles.dest))
+    .pipe(browsersync.stream())
 }
 
 function scripts() {
@@ -52,18 +75,40 @@ function scripts() {
     .pipe(uglify())
     .pipe(concat('main.min.js'))
     .pipe(sourcemaps.write('.'))
+    .pipe(size())
     .pipe(gulp.dest(paths.scripts.dest))
+    .pipe(browsersync.stream())
+}
+
+function img() {
+  return gulp.src(paths.images.src)
+    .pipe(newer(paths.images.dest))
+    .pipe(imagemin({
+      progressive: true
+    }))
+    .pipe(size())
+    .pipe(gulp.dest(paths.images.dest))
 }
 
 function watch() {
+  browsersync.init({
+    server: {
+      baseDir: './dist/'
+    }
+  })
+  gulp.watch(paths.html.dest).on('change', browsersync.reload)
   gulp.watch(paths.styles.src, styles)
   gulp.watch(paths.scripts.src, scripts)
+  gulp.watch(paths.html.src, html)
+  gulp.watch(paths.images.src, img)
 }
 
-const build = gulp.series(clean, gulp.parallel(scripts, styles), watch);
+const build = gulp.series(clean, html, gulp.parallel(scripts, styles, img), watch);
 
 
 exports.clean = clean;
+exports.html = html;
+exports.img = img;
 exports.styles = styles;
 exports.scripts = scripts;
 exports.watch = watch;
